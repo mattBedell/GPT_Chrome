286692e5d70d8296633b5d063d5977a99683d37a
Matt Bedell <matt@bedell.co>
2017-12-31 20:48:27 -0500

    remove old imports

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import Slots from './Containers/Slots_Cont/index.jsx';
 import './app.css'
 
 class App extends Component {
     render() {
         return(
-            <div className='app'>
-                <Slots />
+            <div className='app' >
             </div>
         )
     }
