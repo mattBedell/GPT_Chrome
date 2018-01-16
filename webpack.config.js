daaf32237f69b4f75731efd77fa34623761d728a
Matt Bedell <matt@bedell.co>
2018-01-15 20:25:46 -0500

    add scss to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -139,7 +139,17 @@ const popup = {
       test: /\.css$/,
       exclude: /node_modules/,
       use: ['style-loader', 'css-loader'],
-    }],
+    },
+    {
+      test: /\.scss$/,
+      use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+      }, {
+          loader: "css-loader" // translates CSS into CommonJS
+      }, {
+          loader: "sass-loader" // compiles Sass to CSS
+      }]
+  }],
   },
   plugins: []
 }
