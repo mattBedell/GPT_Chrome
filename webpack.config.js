b67225d5ecf1d9e9a587e6dc2730c459f7ddd243
Matt Bedell <matt@bedell.co>
2018-01-28 16:59:56 -0500

    remove scss loader, add file loader

--- webpack.config.js
+++ webpack.config.js
@@ -141,15 +141,10 @@ const popup = {
       use: ['style-loader', 'css-loader'],
     },
     {
-      test: /\.scss$/,
-      use: [{
-          loader: "style-loader" // creates style nodes from JS strings
-      }, {
-          loader: "css-loader" // translates CSS into CommonJS
-      }, {
-          loader: "sass-loader" // compiles Sass to CSS
-      }]
-  }],
+      test: /\.ttf$/,
+      exclude: /node_modules/,
+      use: 'file-loader',
+    }],
   },
   plugins: []
 }
