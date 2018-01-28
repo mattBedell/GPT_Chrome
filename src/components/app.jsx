5918fab1710ad7bb01a9c443bcce75815c322700
Matt Bedell <matt@bedell.co>
2018-01-28 16:54:40 -0500

    add theme, remove external css

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import './app.css'
+
+import { connect } from 'react-redux';
+
+import { setView } from './../actions/nav';
+
+import { ThemeProvider } from 'styled-components';
+import HTLDarkTheme from './../style/themes/HTLDark';
+import variables from './../style/variables';
+
+
+import Nav from './Nav';
 
 class App extends Component {
-    render() {
-        return(
-            <div className='app' >
-            </div>
-        )
-    }
+  render() {
+    return(
+      <ThemeProvider theme={HTLDarkTheme} >
+        <Nav />
+      </ThemeProvider>
+    )
+  }
 }
+
 
 export default App;
