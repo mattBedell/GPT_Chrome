17f11cb74ca1cad74c3dfa52064027215a2a46a9
Matt Bedell <matt@bedell.co>
2018-01-28 22:11:17 -0500

    add nav and slots

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,24 +1,51 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
-
 import { setView } from './../actions/nav';
+import { getSelectedView } from './../reducers/nav';
 
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import HTLDarkTheme from './../assets/themes/HTLDark';
 
-
 import Nav from './Nav';
+import Slots from './Views/Slots';
+
+
+
+const Spacer = styled.div`
+  width: 100%;
+  height: 2px;
+  background-color: ${props => props.theme.brandPrimary}
+`;
 
 class App extends Component {
   render() {
     return(
-      <ThemeProvider theme={HTLDarkTheme} >
-        <Nav />
+      <ThemeProvider theme={HTLDarkTheme}>
+      <>
+        <Nav
+          selectedValue={this.props.selectedView}
+          setSelected={this.props.setView}
+        />
+        <Spacer />
+        <Slots />
+      </>
       </ThemeProvider>
     )
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    selectedView: getSelectedView(state),
+  }
+}
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    setView: view => dispatch(setView(view)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
