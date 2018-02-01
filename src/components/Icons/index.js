ff953eb8d2e109747f028857e8734f6e1c708b9d
Matt Bedell <matt@bedell.co>
2018-01-31 23:06:01 -0500

    add additional icons

---
+++ src/components/Icons/index.js
@@ -0,0 +1,18 @@
+
+import DivIcon from './Browser';
+import CogIcon from './Gear';
+import ComputerIcon from './Computer';
+import EventsIcon from './Events';
+import EyeIcon from './Eye';
+import WarnNoFillIcon from './WarnNoFill';
+import WarnFillIcon from './WarnFill';
+
+export {
+  DivIcon,
+  CogIcon,
+  ComputerIcon,
+  EventsIcon,
+  EyeIcon,
+  WarnNoFillIcon,
+  WarnFillIcon,
+}
