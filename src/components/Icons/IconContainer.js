2817855faf99b0efd148e78fa57be6f0b630e327
Matt Bedell <matt@bedell.co>
2018-02-04 16:10:00 -0500

    fix container heights

--- src/components/Icons/IconContainer.js
+++ src/components/Icons/IconContainer.js
@@ -7,7 +7,6 @@ import styled from 'styled-components';
 export default styled.div`
 width: ${props => props.width};
 height: ${props => props.height};
-height: 20px;
 transition: transform .2s;
 display: inline-block;
 `;
