ce178f14c8817f54d905fb50a214446b3fe1ff68
Matt Bedell <matt@bedell.co>
2018-02-11 20:30:32 -0500

    add fade HOC

---
+++ src/components/HOCs/WithFade.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Transition } from 'react-transition-group';
+
+import styled from 'styled-components';
+
+
+const FadeContainer = styled.div`
+  opacity: ${props => props.opacity / 100};
+  transition: opacity ${props => props.duration / 1000}s;
+  width: ${props => props.width ? props.width : 'inherit'};
+  height: ${props => props.height ? props.height : 'inherit'};
+`;
+
+export default function(FadedComponent, options = {}) {
+  const defaultOptions = {
+    timeout: 1000,
+    entering: 0,
+    entered: 100,
+    exiting: 0,
+    exited: 0,
+  }
+
+  const transOptions = Object.assign({}, defaultOptions, options);
+  return class extends Component {
+    constructor(props) {
+      super(props);
+    }
+
+    handleEnter(node, isAppearing) {
+      /** on mount <Transition /> passes transition state
+       *  exited, entering
+       * too quickly for a reflow
+       * this triggers a reflow between the two states so an animation happens
+       **/
+      node.scrollTop
+    }
+
+    render() {
+      const childProps = Object.assign({}, this.props)
+      delete childProps.in;
+      return (
+        <Transition
+        timeout={transOptions.timeout}
+        onEnter={this.handleEnter.bind(this)}
+        {...this.props}
+        >
+          {transitionState => {
+          return (
+            <FadeContainer opacity={transOptions[transitionState]} {...childProps} duration={transOptions.timeout}>
+              <FadedComponent {...childProps}/>
+            </FadeContainer>
+          )}}
+        </Transition>
+      )
+    }
+  }
+}
+
+
