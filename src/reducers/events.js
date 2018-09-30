a2ee4502dba372192cc12b1d1f74e5198a8f93c5
Matt Bedell <matt@bedell.co>
2018-09-30 17:42:01 -0400

    fix x is not iterable error

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -14,45 +14,47 @@ const eventHelper = (action, type, subType) => ({
   type: `${type}:${subType}:${action.type}`,
 });
 
-export const events = (state = {}, action) => { // eslint-disable-line
+export const events = (state = [], action) => { // eslint-disable-line
   switch (action.type) {
     case RENDER_ENDED: {
       const typeObj = eventHelper(action, SLOT, RENDER);
       return [
         ...state,
-        Object.assign({},
-          typeObj,
-          action.payload),
+        {
+          ...typeObj,
+          ...action.payload,
+        },
       ];
     }
 
     case DEFINE_SLOT: {
       const typeObj = eventHelper(action, SLOT, GPT);
-
       const { timestamp, slot } = action.payload;
       const { slotId } = slot;
 
       return [
         ...state,
-        Object.assign({
+        {
+          ...typeObj,
           timestamp,
           slotId,
-        }, typeObj),
+        },
       ];
     }
 
     case SET_TARGETING: {
       const typeObj = eventHelper(action, SLOT, GPT);
 
-      const { timestamp, slotId } = action.payload;
+      const { timestamp, slotId, targeting } = action.payload;
 
       return [
         ...state,
-        Object.assign({
+        {
+          ...typeObj,
           timestamp,
           slotId,
-          targeting: action.payload.targeting,
-        }, typeObj),
+          targeting,
+        },
       ];
     }
 
