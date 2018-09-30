df8019fa64969e26343853c0f9540b507353dbaf
Matt Bedell <matt@bedell.co>
2018-09-30 17:40:28 -0400

    simplify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,17 @@
+const { DefinePlugin, ExtendedAPIPlugin } = require('webpack');
+const OnBuildWebpackPlugin = require('on-build-webpack');
 
 const { execSync } = require('child_process');
 const path = require('path');
 
-const webpack = require('webpack');
-const MinifyPlugin = require('babel-minify-webpack-plugin');
-const WebpackOnBuildPlugin = require('on-build-webpack');
-const merge = require('webpack-merge');
-
-const SCRIPTS_DIR = path.join(__dirname, 'src/scripts');
-const EXTENSION_DIR = path.join(__dirname, 'extension/dist');
-const POPUP_ENTRY = path.join(__dirname, 'src/index.jsx');
-
-function getActiveTab() {
-  const activeTab = execSync('chrome-cli info');
-  return activeTab.toString().split('\n')[0].split(': ')[1];
-}
-
-const production = {
-  devtool: 'source-map',
-  plugins: [
-    new MinifyPlugin({}, { test: /\.(js|jsx)$/i }),
-  ],
-};
-
-const hot = {
-  entry: [
-    'webpack-dev-server/client?http://localhost:3001',
-    'webpack/hot/only-dev-server',
-  ],
-  output: {
-    publicPath: '/dist',
-  },
-  module: {
-    rules: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: [{
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            ['@babel/preset-es2015', { modules: false }],
-          ],
-          plugins: ['react-hot-loader/babel'],
-        },
-      }],
-    }],
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin(),
-  ],
-};
-
-const scripts = {
-  devtool: 'eval-source-map',
+const SCRIPTS_DIR = path.resolve('src', 'scripts');
+const EXTENSION_DIR = path.resolve('extension', 'dist');
+const config = {
   entry: {
     background: `${SCRIPTS_DIR}/background.js`,
     content_script: `${SCRIPTS_DIR}/content_script.js`,
     inject_script: `${SCRIPTS_DIR}/inject_script.js`,
+    popup: path.resolve('src', 'components', 'index.jsx'),
   },
   output: {
     path: EXTENSION_DIR,
@@ -66,89 +20,40 @@ const scripts = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js(x|)$/,
         exclude: /node_modules/,
         use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                ['@babel/preset-es2015'],
-                ['@babel/preset-stage-0'],
-              ],
-            },
-          },
-          'eslint-loader',
+          { loader: 'babel-loader' },
+          { loader: 'eslint-loader' },
         ],
+      },
+      {
+        test: /\.ttf$/,
+        exclude: /node_modules/,
+        use: 'file-loader',
       },
     ],
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('development'),
-      },
-    }),
-    new webpack.ExtendedAPIPlugin(),
-    new WebpackOnBuildPlugin(() => {
-      const tabId = getActiveTab();
-      execSync(`chrome-cli reload -t ${tabId}`);
+    new DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
     }),
   ],
 };
 
-const popup = {
-  devtool: 'eval-source-map',
-  entry: [POPUP_ENTRY],
-  output: {
-    path: EXTENSION_DIR,
-    filename: 'popup.js',
-  },
-  module: {
-    rules: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: [{
-        loader: 'babel-loader',
-        options: {
-          plugins: [
-            '@babel/plugin-transform-runtime',
-          ],
-          presets: [
-            ['@babel/preset-es2015'],
-            ['@babel/preset-stage-0'],
-            ['@babel/preset-react'],
-          ],
-        },
-      }],
-    },
-    {
-      test: /\.css$/,
-      exclude: /node_modules/,
-      use: ['style-loader', 'css-loader'],
-    },
-    {
-      test: /\.ttf$/,
-      exclude: /node_modules/,
-      use: 'file-loader',
-    }],
-  },
-  plugins: [],
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.plugins = [
+      ...config.plugins,
+      new ExtendedAPIPlugin(),
+      new OnBuildWebpackPlugin(() => {
+        // reload the active chrome tab
+        const activeTab = execSync('chrome-cli info');
+        const tabId = activeTab.toString().split('\n')[0].split(': ')[1];
+        execSync(`chrome-cli reload -t ${tabId}`);
+      }),
+    ];
+  }
+
+  return config;
 };
-
-const configs = {
-  production,
-  hot,
-  scripts,
-  popup,
-};
-
-
-module.exports = ((env = {}) => {
-  const { BUILD_TYPE, NODE_ENV } = env;
-  const base = configs[BUILD_TYPE];
-
-  return merge.smartStrategy({ entry: 'prepend', plugins: 'replace' })(base, configs[NODE_ENV] || {});
-});
-
-// "chrome-cli reload -t $((chrome-cli list tabs | grep -q ]\\ Extensions$ || chrome-cli open chrome://extensions; chrome-cli list tabs) | grep ]\\ Extensions$ | perl -pe 's/(.*(?=:):)|\\[//g' | sed -e 's/\\] Extensions//g') || exit 0"
